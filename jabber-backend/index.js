f0146f428324acaac8178eda013b8ea843282e36
Lakshman Karthick <lakshmankarthickt@gmail.com>
2024-06-22 22:04:32 +0530

    back modified

--- jabber-backend/index.js
+++ jabber-backend/index.js
@@ -8,13 +8,13 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "*",
         methods: ["GET", "POST"]
     }
 });
 
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
